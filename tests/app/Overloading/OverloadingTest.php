29655637231e2bcdff37030684842b9b187621e3
Edwin Arley Rincon Henao <shortymolinari@gmail.com>
2019-01-26 11:41:32 -0500

    adding an overloading test

--- tests/app/Overloading/OverloadingTest.php
+++ tests/app/Overloading/OverloadingTest.php
@@ -18,10 +18,9 @@ class OverloadingTest extends TestCase
      * @group overloading
      * @test
      */
-    public function a_property_can_be_created_without_using_the_set_method_()
+    public function a_property_can_be_created_without_using_the_set_method()
     {
         $this->overloading->name = 'Edwin Rincón';
-        //$this->assertClassHasAttribute('name', Overloading::class);
         $this->assertSame('Edwin Rincón', $this->overloading->__get('name'));
     }
 
@@ -29,10 +28,19 @@ class OverloadingTest extends TestCase
      * @group overloading
      * @test
      */
-    public function a_property_can_be_created_using_the_set_method_()
+    public function a_property_can_be_created_using_the_set_method()
     {
         $this->overloading->__set('name', 'Edwin Rincón');
-        //$this->assertClassHasAttribute('name', Overloading::class);
         $this->assertSame('Edwin Rincón', $this->overloading->__get('name'));
+    }
+
+    /**
+     * @group overloading
+     * @test
+     */
+    public function the_property_can_be_an_array()
+    {
+        $this->overloading->__set('skills', ['PHP', 'javascript', 'HTML', 'CSS']);
+        $this->assertContains('HTML', $this->overloading->__get('skills'));
     }
 }
